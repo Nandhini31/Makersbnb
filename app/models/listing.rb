6fb68a6f52c6def69b14229814b180bd6e5ea396
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-08-09 12:27:05 +0100

    Resolve merging conflicts

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,11 +4,7 @@ class Listing
   property :id, Serial
   property :title, String
   property :description, Text
-<<<<<<< HEAD
   # property :address, Text
-=======
-  property :address, Text
->>>>>>> master
   property :price_per_night, Integer
 
   belongs_to :user
--- spec/features/listing_spec.rb
+++ spec/features/listing_spec.rb
@@ -9,13 +9,8 @@ feature 'Listings' do
     visit('/listings')
     within 'ul#listings' do
       expect(page).to have_content('Nice and charming place to stay')
-<<<<<<< HEAD
       # expect(page).to have_content('No 4, london street')
       # expect(page).to have_content(50)
-=======
-      expect(page).to have_content('No 4, london street')
-      expect(page).to have_content(50)
->>>>>>> master
     end
   end
 end
